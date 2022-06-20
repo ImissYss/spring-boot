e411a4b6db5210fdd2b9070b90e352e83ed07043
maitiendat <dathust165955@gmail.com>
2022-06-20 20:38:48 +0700

    spring error handing

---
+++ spring-rest-error-handing/src/test/java/com/maidat/springresterrorhanding/SpringRestErrorHandingApplicationTests.java
@@ -0,0 +1,13 @@
+package com.maidat.springresterrorhanding;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringRestErrorHandingApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
